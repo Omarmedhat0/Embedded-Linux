e65277a6146e3f42dfef2b606e771134863d1e5e
Omar Medhat <omar@Omar.Medhat>
2023-11-22 23:47:19 +0200

    AdminLinux_Task1 V.1

---
+++ AdminLinux_Task1/AdminLinux_Task1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir ~/myDirectory
+mkdir ~/myDirectory/secondDirectory
+cd ~/myDirectory/secondDirectory
+touch myNotePaper
+cp myNotePaper ../
+cd ..
+mv myNotePaper myOldNotePaper
+
