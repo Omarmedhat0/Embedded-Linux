7d6a534e5e0771bad5646d24d0614422f87ff80b
Omar Medhat <omar@Omar.Mdhat>
2023-12-19 22:50:22 +0200

    Task1_Embedded_Linux

---
+++ Task1_Embedded_Linux/DynamicLibrary_Lab/Include/subtraction.h
@@ -0,0 +1,4 @@
+#ifndef SUBTRACTION_H
+#define SUBTRACTION_H
+float sub (float num1 ,float num2);
+#endif // SUBTRACTION_H
---
+++ Task1_Embedded_Linux/staticLibrary_Lab/Include/multiplication.h
@@ -0,0 +1,4 @@
+#ifndef MODULUS_H
+#define MODULUS_H
+float multipication (float num1 , float num2);
+#endif // MODULUS_H
